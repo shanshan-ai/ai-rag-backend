805d845c7658a5d8229cf0555249a24933155635
wenwen <wenwen2006123@gmail.com>
2026-07-30 18:31:34 +0800

    feat: add vector database similarity search function

--- rag.py
+++ rag.py
@@ -31,14 +31,35 @@ def create_vector_store(chunks):
     print("向量数据库构建完成! - rag.py:31")
     return vector_store
 
+def search_knowledge_base(query: str,k: int = 2):
+    """根据用户问题，从向量数据库中检索最相关的文本块"""
+    print(f"\n正在加载嵌入模型并连接向量数据库以检索问题： ‘{query} - rag.py:36")   
+
+    #1.初始化相同的嵌入模型
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+    #2.从本地文件夹加载持久化的 Chroma 数据库
+    vector_store = Chroma(
+        persist_directory="./chroma_db", 
+        embedding_function=embeddings
+    )
+
+    #3.相似度检索(Similarity Search)
+    results = vector_store.similarity_search(query, k=k)
+
+    print(f"\n检索结果(Top {k}) - rag.py:50")
+    for i,doc in enumerate(results):
+        print(f"[{i+1}] 内容: {doc.page_content} - rag.py:52")
+    return results
+
 if __name__ == "__main__":
-    #测试一下切分效果
     file_path = "test.txt"  
 
-    #1.切分文档
+    #1.切分文档并存入向量数据库
     chunks = process_document(file_path)
-    print(f"总共切分出了 {len(chunks)} 块文本 (chunks):\n - rag.py:40")
-
-    #2.存入向量数据库
     vector_store = create_vector_store(chunks)
-   
+
+    #2.测试检索功能
+    test_query = "什么是人工智能？"
+    search_knowledge_base(test_query, k=2)
+                                   
